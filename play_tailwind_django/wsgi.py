d3da0e74af05c6796a5d664157c22c926ecddc4a
momenurmaruf2742 <momenur.maruf@gmail.com>
2024-09-09 00:50:17 +0600

    mongodb added

--- play_tailwind_django/wsgi.py
+++ play_tailwind_django/wsgi.py
@@ -14,3 +14,4 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'play_tailwind_django.settings')
 
 application = get_wsgi_application()
+app = application
